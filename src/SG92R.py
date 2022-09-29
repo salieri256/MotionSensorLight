9a971d542972299e074461b064e0653e2cd397a4
さりえり <nakakou201250@gmail.com>
2022-09-29 15:25:17 +0900

    命名を変更 #2 (#3)
    close #2

--- src/SG92R.py
+++ src/SG92R.py
@@ -1,7 +1,7 @@
 from machine import PWM, Pin
 from PicoGpioNumber import PicoGpioNumber
 from SG92RRotationDegree import SG92RRotationDegree
-from SG92RPulseWidthPerSecond import SG92RPulseWidthPerSecond
+from SG92RPulseWidthPerMilliSecond import SG92RPulseWidthPerMilliSecond
 
 class SG92R:
     __FREQUENCY = 50
@@ -11,6 +11,6 @@ class SG92R:
         self.__pwm_pin.freq(SG92R.__FREQUENCY)
     
     def rotate(self, degree: SG92RRotationDegree):
-        duty_width_per_s = SG92RPulseWidthPerSecond.calc_duty_width(degree)
+        duty_width_per_s = SG92RPulseWidthPerMilliSecond.calc_duty_width(degree)
         duty_cycle_per_ns = duty_width_per_s.value * 1_000_000
         self.__pwm_pin.duty_ns( int(duty_cycle_per_ns) )
