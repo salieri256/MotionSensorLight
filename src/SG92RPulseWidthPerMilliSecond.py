405cbe703a8d65a7b4bef0238e9b3b730610ae3f
さりえり <nakakou201250@gmail.com>
2022-09-29 15:20:20 +0900

    命名を変更 #2

--- src/SG92RPulseWidthPerMilliSecond.py
+++ src/SG92RPulseWidthPerMilliSecond.py
@@ -1,11 +1,11 @@
 from SG92RRotationDegree import SG92RRotationDegree
 
-class SG92RPulseWidthPerSecond:
+class SG92RPulseWidthPerMilliSecond:
     __MIN_VALUE = 0.5
     __MAX_VALUE = 2.4
     
     def __init__(self, value: float):
-        if value < SG92RPulseWidthPerSecond.__MIN_VALUE or SG92RPulseWidthPerSecond.__MAX_VALUE < value:
+        if value < SG92RPulseWidthPerMilliSecond.__MIN_VALUE or SG92RPulseWidthPerMilliSecond.__MAX_VALUE < value:
             raise Exception('SG92R-pulse-width-per-second is incorrect.')
         
         self.__value = value
@@ -16,9 +16,9 @@ class SG92RPulseWidthPerSecond:
     
     @staticmethod
     def calc_duty_width(degree: SG92RRotationDegree):
-        pulse_width_amplitude = SG92RPulseWidthPerSecond.__MAX_VALUE - SG92RPulseWidthPerSecond.__MIN_VALUE
+        pulse_width_amplitude = SG92RPulseWidthPerMilliSecond.__MAX_VALUE - SG92RPulseWidthPerMilliSecond.__MIN_VALUE
         degree_amplitude = SG92RRotationDegree.MAX_VALUE - SG92RRotationDegree.MIN_VALUE
         pulse_width_per_degree = pulse_width_amplitude / degree_amplitude
-        duty_width = pulse_width_per_degree * (degree.value - SG92RRotationDegree.MIN_VALUE) + SG92RPulseWidthPerSecond.__MIN_VALUE
-        return SG92RPulseWidthPerSecond(duty_width)
+        duty_width = pulse_width_per_degree * (degree.value - SG92RRotationDegree.MIN_VALUE) + SG92RPulseWidthPerMilliSecond.__MIN_VALUE
+        return SG92RPulseWidthPerMilliSecond(duty_width)
         
